50bc46cbc3276cd8bd8fe1084ed10832f93f1a7c
Abhishek <Abhishek@millicent.in>
2024-03-26 10:57:40 +0530

    methods added for cors all origin

--- index.js
+++ index.js
@@ -11,21 +11,27 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+    methods: ["PUT", "POST", "GET", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 
-app.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
+// app.get("/", (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Max-Age", "1800");
+//   res.setHeader("Access-Control-Allow-Headers", "content-type");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+//   );
 
-  // Proceed to other route handling
-});
+//   // Proceed to other route handling
+// });
 
 app.use("/api", login);
 app.use("/api/sectors", sector);
