5622fbcd09e56143a1a41dbadd739c228ef84655
Abhishek <Abhishek@millicent.in>
2024-03-26 10:48:58 +0530

    Cors header applied

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+
+  // Proceed to other route handling
+});
+
 app.use("/api", login);
 app.use("/api/sectors", sector);
 app.use("/api/industries", industry);
