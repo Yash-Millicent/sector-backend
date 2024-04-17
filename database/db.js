f21532896ab90ca641ef62b0212797f36ac10671
Yash <yash@millicent.in>
2024-04-17 16:28:49 +0530

    Db changed to shreeja_v2-0.1

--- database/db.js
+++ database/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const USERNAME = process.env.DB_USERNAME || "user";
 const PASSWORD = process.env.DB_PASSWORD || "user";
 const HOST = process.env.DB_HOST || "203.212.222.191";
-const DATABASE = process.env.DB_DATABASE || "shreeja_v2.0.1";
+const DATABASE = process.env.DB_DATABASE || "shreeja_v2-0.1";
 // Initialize Sequelize with your database credentials
 const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   host: HOST,
