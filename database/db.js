dbc42d25500391d01de628fb98b7a704950d6eaa
Abhi7millicent <153972197+Abhi7millicent@users.noreply.github.com>
2024-04-17 15:43:07 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 const HOST = process.env.DB_HOST;
-const DATABASE = process.env.DB_DATABASE;
+const DATABASE = "shreeja_v2-0.1";
 // Initialize Sequelize with your database credentials
 const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   host: HOST,
