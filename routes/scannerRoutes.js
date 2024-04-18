64064c5b9ec8b73edcec8e8cb5aae6766701e0aa
Yash <yash@millicent.in>
2024-04-18 11:37:25 +0530

    Periodic high low api created and db name changed

--- routes/scannerRoutes.js
+++ routes/scannerRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   getDetailsBySectorOrIndices,
+  getPeriodicHighLow,
   getTechnicalTableData,
 } from "../controllers/scannerController.js";
 
 const router = express.Router();
 
+router.get("/:tableTab/:sectorOrIndices", getPeriodicHighLow);
 router.get("/:headerType/:sectorOrIndices", getDetailsBySectorOrIndices);
 router.get("/technical-data", getTechnicalTableData);
 export default router;
