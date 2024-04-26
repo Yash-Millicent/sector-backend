e323f7253d4e0e2a7a99dae1ad6e028cba7eb6ab
Yash <yash@millicent.in>
2024-04-26 11:54:56 +0530

    timeout changed to 2 mins

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.use(
   })
 );
 
-// app.set("serverTimeout", 120000);
-app.use(function (req, res, next) {
-  req.setTimeout(500000, function () {
-    res.json({ error: "Server timeout !!" });
-  });
-  next();
-});
+app.set("serverTimeout", 120000);
+// app.use(function (req, res, next) {
+//   req.setTimeout(500000, function () {
+//     res.json({ error: "Server timeout !!" });
+//   });
+//   next();
+// });
 
 app.use("/api", login);
 app.use("/api/sectors", sector);
