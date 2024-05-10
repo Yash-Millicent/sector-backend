de44955e50b16dd23d2ce8989475389f54589693
Yash <yash@millicent.in>
2024-05-10 17:04:34 +0530

    Database connection timeout set to 2 minutes

--- database/db.js
+++ database/db.js
@@ -8,12 +8,18 @@ const USERNAME = process.env.DB_USERNAME || "user";
 const PASSWORD = process.env.DB_PASSWORD || "user";
 const HOST = process.env.DB_HOST || "203.212.222.191";
 const DATABASE = process.env.DB_DATABASE || "shreeja_v2.0.1";
+
+const CONNECTION_TIMEOUT = 120000;
+
 // Initialize Sequelize with your database credentials
 const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   host: HOST,
   dialect: "mysql",
   dialectModule: mysql2,
   storage: DATABASE,
+  dialectOptions: {
+    connectTimeout: CONNECTION_TIMEOUT,
+  },
 });
 
 export default sequelize;
