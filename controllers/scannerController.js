d3db1031a16cd6465a072a1fc3f1c0fd5655b875
Yash <yash@millicent.in>
2024-05-10 17:32:23 +0530

    API optimized

--- controllers/scannerController.js
+++ controllers/scannerController.js
@@ -1,44 +1,81 @@
 import sequelize from "../database/db.js";
 
 // Get list of sectors
+// export const getDetailsBySectorOrIndices = async (req, res) => {
+//   try {
+//     const headerType = req.params.headerType;
+//     const type = req.params.type;
+//     const sectorOrIndices = req.params.sectorOrIndices;
+//     const exch = req.params.exch;
+//     console.log("header", headerType, type, sectorOrIndices, exch);
+//     const parts = sectorOrIndices.split(/[-,:]/);
+//     // Extract the parts
+//     const selectedType = parts[0];
+//     const number = parts[1];
+//     let sectorId = 0;
+//     let indices = 0;
+//     if (selectedType === "sector") {
+//       sectorId = number;
+//     }
+//     if (selectedType === "index") {
+//       indices = number;
+//     }
+//     sequelize
+//       .query(
+//         "CALL `sp_get_scanner_overview_v1`(:headerType, :type, :sectorId, :indices, :exch)",
+//         {
+//           replacements: {
+//             headerType,
+//             type,
+//             sectorId,
+//             indices,
+//             exch,
+//           },
+//         }
+//       )
+//       .then(function (companies) {
+//         res.json(companies);
+//       });
+//   } catch (error) {
+//     console.error("Error in getDetailsBySectorOrIndices:", error);
+//     res.status(500).json({ message: error });
+//   }
+// };
+
 export const getDetailsBySectorOrIndices = async (req, res) => {
   try {
-    const headerType = req.params.headerType;
-    const type = req.params.type;
-    const sectorOrIndices = req.params.sectorOrIndices;
-    const exch = req.params.exch;
+    const { headerType, type, sectorOrIndices, exch } = req.params;
     console.log("header", headerType, type, sectorOrIndices, exch);
+
     const parts = sectorOrIndices.split(/[-,:]/);
-    // Extract the parts
     const selectedType = parts[0];
     const number = parts[1];
     let sectorId = 0;
     let indices = 0;
+
     if (selectedType === "sector") {
       sectorId = number;
-    }
-    if (selectedType === "index") {
+    } else if (selectedType === "index") {
       indices = number;
     }
-    sequelize
-      .query(
-        "CALL `sp_get_scanner_overview_v1`(:headerType, :type, :sectorId, :indices, :exch)",
-        {
-          replacements: {
-            headerType,
-            type,
-            sectorId,
-            indices,
-            exch,
-          },
-        }
-      )
-      .then(function (companies) {
-        res.json(companies);
-      });
+
+    const companies = await sequelize.query(
+      "CALL `sp_get_scanner_overview_v1`(:headerType, :type, :sectorId, :indices, :exch)",
+      {
+        replacements: {
+          headerType,
+          type,
+          sectorId,
+          indices,
+          exch,
+        },
+      }
+    );
+
+    res.json(companies);
   } catch (error) {
     console.error("Error in getDetailsBySectorOrIndices:", error);
-    res.status(500).json({ message: error });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
