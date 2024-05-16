8e9a61affd7d30dddb089a9de7ab8a638cb76dbd
Yash <yash@millicent.in>
2024-05-16 19:15:06 +0530

    Db ip changed

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const USERNAME = process.env.DB_USERNAME || "user";
 const PASSWORD = process.env.DB_PASSWORD || "user";
-const HOST = process.env.DB_HOST || "192.168.1.2";
+const HOST = process.env.DB_HOST || "203.212.222.191";
 const DATABASE = process.env.DB_DATABASE || "shreeja_v2.0.1";
 
 const CONNECTION_TIMEOUT = 120000;
